c24df0104a153ce8ec07038cabfff3b3021e2b5a
imj22 <98693440+imj22@users.noreply.github.com>
2022-05-24 17:28:16 +0200

    completed deliverables

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,17 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
 
+    def give_freebie(dev, item_name, value)
+        freebie = Freebie.create(item_name: item_name, value: value)
+        freebie.dev = dev
+        freebie.company = self
+        freebie.save
+    end
+
+    def self.oldest_company
+       self.find_by(founding_year: self.all.map {|company| company.founding_year}.min)
+    end
 end
+
+
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,23 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def recieved_one?(item_name)
+        self.freebies.find {|freebie| freebie.item_name == item_name} == nil ? false : true
+        # i think there is a ruby method that will do code above for me...
+    end
+
+    def give_away(dev, freebie)
+        freebie_found = self.freebies.find {|self_freebie| self_freebie == freebie}
+        # freebie_found == nil ? "this does not belong to you" : freebie_found.dev = dev
+        # freebie_found.save
+
+        if freebie_found == nil
+            "this does not belong to you"
+        else
+            freebie_found.dev = dev
+            freebie_found.save
+        end
+    end
 
 end
